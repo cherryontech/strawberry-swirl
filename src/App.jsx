073cf7645a1e6935ba675e73c0ec0450749425f1
Kelly <kellywu18029@gmail.com>
2025-10-17 17:35:08 -0400

    Testing netlify deploy for the dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ApplicationForm from "./components/ApplicationForm";
 function App() {
   return (
     <div>
+      <h1>Testing Netlify Deploy for Dev Branch</h1>
       <ApplicationForm />
     </div>
   );
