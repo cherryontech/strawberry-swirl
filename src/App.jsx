6f395e23f93fedabd987d3575126c7e0d52e2a15
Kelly <kellywu18029@gmail.com>
2025-10-18 15:18:08 -0400

    Testing Netlify's Deploy Preview

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ApplicationForm from "./components/ApplicationForm";
 function App() {
   return (
     <div>
+      <h1>Testing Netlify Deploy Preview</h1>
       <ApplicationForm />
     </div>
   );
