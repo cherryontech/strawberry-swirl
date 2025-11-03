4d6a89ea1d5c0682de082cce71ec194db17d3dff
Kelly <kellywu18029@gmail.com>
2025-11-03 16:13:48 -0500

    added protectedContent prop in AuthGuard from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
@@ -8,6 +9,7 @@ import Home from "./pages/Home.jsx";
 import Onboarding from "./pages/Onboarding.jsx";
 import Start from "./pages/Start.jsx";
 import Resources from "./pages/Resources.jsx";
+import JobTracking from "./pages/JobTracking.jsx";
 
 import OnboardingGuard from "./guards/OnboardingGuard.jsx";
 import AuthGuard from "./guards/AuthGuard.jsx";
@@ -16,16 +18,13 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <AuthGuard>
-        <App />
-      </AuthGuard>
+      <AuthGuard protectedContent={<App />} />
     ),
     children: [
       { index: true, element: <Home /> },
       { path: "start", element: <Start /> },
       { path: "resources", element: <Resources /> },
-      { path: "/", element: <App /> },
-      { path: "/jobtracking", element: <JobTracking /> },
+      { path: "jobtracking", element: <JobTracking /> },
     ],
   },
   {
@@ -43,5 +42,3 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </StrictMode>,
 );
-
-export default Layout;
