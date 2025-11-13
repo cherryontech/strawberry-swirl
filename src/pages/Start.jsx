433053dca3b42fa5c831c5e5c885b58c200b1d4d
Kelly <kellywu18029@gmail.com>
2025-11-12 23:20:28 -0500

    re-styled start page

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -3,28 +3,27 @@ import { GoArrowRight } from "react-icons/go";
 
 const Start = () => {
   return (
-    <div className="bg-softPink w-full h-screen">
-      <div className="flex flex-col justify-center items-center h-screen">
-        <div>
-          <p className="font-lora text-6xl">Welcome to</p>
-          <h1 className="font-lora text-9xl mb-40">[name of app]</h1>
+    <div className="bg-backgroundColor w-full h-screen flex flex-col">
+      <header className="flex items-center gap-2 p-4">
+        <img src="/logo.png" alt="logo" className="w-20 h-20" />
+        <h2 className="text-deepOrange">Motifly</h2>
+      </header>
+
+      <main className="flex flex-col justify-center items-center flex-1">
+        <div className="text-left">
+          <p className="font-lora text-6xl text-bordeaux">Welcome to</p>
+          <h1 className="font-lora text-9xl mb-32 text-bordeaux">Motifly</h1>
         </div>
-        <div className="flex z-10">
-          <p className="font-lora text-6xl mb-6 me-4">lets get started!</p>
-          <a href="/" className="mt-2">
-            <div className="bg-gray-50 rounded-2xl shadow-lg px-2">
-              <GoArrowRight size={52} />
-            </div>
+
+        <div className="flex items-center z-10">
+          <p className="font-lora text-6xl me-4 text-deepOrange">
+            let&apos;s get started!
+          </p>
+          <a href="/" className="mt-2 text-deepOrange">
+            <GoArrowRight size={52} />
           </a>
         </div>
-        <div className="absolute bottom-50 right-20 bg-gray-50 rounded-4xl shadow-lg px-8 py-12">
-          <p className="font-lora text-center">
-            Motivation Buddy
-            <br />
-            Logo
-          </p>
-        </div>
-      </div>
+      </main>
     </div>
   );
 };
