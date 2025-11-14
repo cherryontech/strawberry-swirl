d9e05976a5f25963f2e04f807706b571f2e1d080
Shweta Tiwari <krishnast16@gmail.com>
2025-11-13 23:10:43 -0500

    Added job boards and article links to resource page

---
+++ src/utils/jobPortalsData.js
@@ -0,0 +1,155 @@
+const jobLinksData = {
+  Fintech: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/fintech-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Fintech-jobs.html",
+    },
+    {
+      jobPortalName: "Wellfound (ex-AngelList)",
+      jobPortalLink: "https://wellfound.com/ ",
+    },
+    { jobPortalName: "Otta", jobPortalLink: "https://otta.com/" },
+    {
+      jobPortalName: "eFinancialCareers",
+      jobPortalLink: ": https://www.efinancialcareers.com/",
+    },
+  ],
+
+  Marketing: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/marketing-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Marketing-jobs.html",
+    },
+    {
+      jobPortalName: "The Muse",
+      jobPortalLink: ": https://www.themuse.com/jobs?q=marketing",
+    },
+    {
+      jobPortalName: "Wellfound (ex-AngelList)",
+      jobPortalLink: "https://wellfound.com/",
+    },
+    {
+      jobPortalName: "Glassdoor",
+      jobPortalLink:
+        "https://www.glassdoor.com/Job/marketing-jobs-SRCH_KO0,9.htm",
+    },
+  ],
+
+  Sales: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/sales-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Sales-jobs.html",
+    },
+    {
+      jobPortalName: "Glassdoor",
+      jobPortalLink: "https://www.glassdoor.com/Job/sales-jobs-SRCH_KO0,5.htm",
+    },
+    { jobPortalName: "Otta", jobPortalLink: ": https://otta.com/" },
+    {
+      jobPortalName: "HubSpot Careers",
+      jobPortalLink: "https://www.hubspot.com/careers",
+    },
+  ],
+
+  PM: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/product-manager-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Product-Manager-jobs.htm",
+    },
+    {
+      jobPortalName: "Mind the Product Jobs",
+      jobPortalLink: "https://jobs.mindtheproduct.com/",
+    },
+    {
+      jobPortalName: "Wellfound (ex-AngelList)",
+      jobPortalLink: "https://wellfound.com/",
+    },
+    {
+      jobPortalName: "Glassdoor",
+      jobPortalLink:
+        "https://www.glassdoor.com/Job/product-manager-jobs-SRCH_KO0,15.htm",
+    },
+  ],
+
+  SWE: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/software-engineer-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Software-Engineer-jobs.html",
+    },
+    {
+      jobPortalName: "Stack Overflow Jobs",
+      jobPortalLink: "https://stackoverflow.com/jobs",
+    },
+    {
+      jobPortalName: "Wellfound (ex-AngelList)",
+      jobPortalLink: "https://wellfound.com/",
+    },
+    { jobPortalName: "GitHub Jobs", jobPortalLink: "https://jobs.github.com/" },
+  ],
+
+  FullStack: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/full-stack-developer-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Full-Stack-Developer-jobs.html",
+    },
+    {
+      jobPortalName: "Stack Overflow Jobs",
+      jobPortalLink: "https://stackoverflow.com/jobs",
+    },
+    {
+      jobPortalName: "Wellfound (ex-AngelList)",
+      jobPortalLink: "https://wellfound.com/",
+    },
+    { jobPortalName: "Remote OK", jobPortalLink: "https://remoteok.com/" },
+  ],
+
+  AI_ML: [
+    {
+      jobPortalName: "LinkedIn Jobs",
+      jobPortalLink: "https://www.linkedin.com/jobs/machine-learning-jobs",
+    },
+    {
+      jobPortalName: "Indeed",
+      jobPortalLink: "https://www.indeed.com/q-Machine-Learning-jobs.htm",
+    },
+    {
+      jobPortalName: "Kaggle Jobs",
+      jobPortalLink: "https://www.kaggle.com/jobs",
+    },
+    {
+      jobPortalName: "Wellfound (ex-AngelList)",
+      jobPortalLink: "https://wellfound.com/",
+    },
+    {
+      jobPortalName: "Glassdoor",
+      jobPortalLink:
+        "https://www.glassdoor.com/Job/machine-learning-jobs-SRCH_KO0,16.htm",
+    },
+  ],
+};
+
+export default jobLinksData;
