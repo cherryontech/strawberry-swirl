5687c9097fc4c179545af37228b33f69f6921824
Kelly <kellywu18029@gmail.com>
2025-11-20 18:49:47 -0500

    Added notes functionalities

--- src/components/JobApplication.jsx
+++ src/components/JobApplication.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import PropTypes from "prop-types";
 
 import { FaRegStickyNote } from "react-icons/fa";
@@ -14,41 +16,56 @@ const JobApplication = ({
   onEdit,
   onDelete,
 }) => {
+  const [showNotes, setShowNotes] = useState(false);
+
+  const handleShowNotes = () => {
+    setShowNotes((prev) => !prev);
+  };
+
   return (
-    <div className="flex items-center w-full pt-4 pb-2 border-b border-backgroundColor">
-      <p className="w-1/4 truncate">{company}</p>
-      <p className="w-3/8 truncate">{role}</p>
-      <p className="w-1/6 truncate">{dateApplied}</p>
-      <p className="w-1/6 truncate">{status}</p>
-      <div className="flex items-center gap-2 w-24 justify-end">
-        {notes && (
-          <button>
-            <FaRegStickyNote
+    <div className="flex flex-col pt-4 pb-2 border-b border-backgroundColor">
+      <div className="flex items-center w-full">
+        <p className="w-1/4 truncate">{company}</p>
+        <p className="w-3/8 truncate">{role}</p>
+        <p className="w-1/6 truncate">{dateApplied}</p>
+        <p className="w-1/6 truncate">{status}</p>
+        <div className="flex items-center gap-2 w-24 justify-end">
+          {notes && (
+            <button onClick={handleShowNotes}>
+              <FaRegStickyNote
+                size={20}
+                className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
+                title="Notes"
+              />
+            </button>
+          )}
+          <button
+            onClick={() =>
+              onEdit({ id, company, role, dateApplied, status, notes })
+            }
+          >
+            <LuPencil
               size={20}
               className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
-              title="Notes"
+              title="Edit"
             />
           </button>
-        )}
-        <button
-          onClick={() =>
-            onEdit({ id, company, role, dateApplied, status, notes })
-          }
-        >
-          <LuPencil
-            size={20}
-            className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
-            title="Edit"
-          />
-        </button>
-        <button onClick={() => onDelete(id)}>
-          <FaRegTrashAlt
-            size={20}
-            className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
-            title="Delete"
-          />
-        </button>
+          <button onClick={() => onDelete(id)}>
+            <FaRegTrashAlt
+              size={20}
+              className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
+              title="Delete"
+            />
+          </button>
+        </div>
       </div>
+
+      {showNotes && (
+        <div className="w-full pt-4">
+          <p className="font-semibold text-deepOrange">Notes</p>
+          <p className="whitespace-pre-line wrap-break-word">{notes}</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/SavedJob.jsx
+++ src/components/SavedJob.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import PropTypes from "prop-types";
 
 import { FaRegStickyNote } from "react-icons/fa";
@@ -5,36 +7,54 @@ import { LuPencil } from "react-icons/lu";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const SavedJob = ({ id, company, role, link, notes, onEdit, onDelete }) => {
+  const [showNotes, setShowNotes] = useState(false);
+
+  const handleShowNotes = () => {
+    setShowNotes((prev) => !prev);
+  };
+
   return (
-    <div className="flex items-center gap-2 w-full pt-4 pb-2 border-b border-backgroundColor">
-      <p className="w-1/3 truncate">{company}</p>
-      <p className="w-1/3 truncate">{role}</p>
-      <p className="w-1/3 truncate">{link}</p>
-      <div className="flex items-center gap-2 w-24 justify-end">
-        {notes && (
-          <button>
-            <FaRegStickyNote
+    <div className="flex flex-col pt-4 pb-2 border-b border-backgroundColor">
+      <div className="flex items-center w-full">
+        <p className="w-1/3 truncate">{company}</p>
+        <p className="w-1/3 truncate">{role}</p>
+        <p className="w-1/3 truncate">{link}</p>
+
+        <div className="flex items-center gap-2 w-24 justify-end">
+          {notes && (
+            <button onClick={handleShowNotes}>
+              <FaRegStickyNote
+                size={20}
+                className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
+                title="Notes"
+              />
+            </button>
+          )}
+
+          <button onClick={() => onEdit({ id, company, role, link, notes })}>
+            <LuPencil
               size={20}
               className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
-              title="Notes"
+              title="Edit"
             />
           </button>
-        )}
-        <button onClick={() => onEdit({ id, company, role, link, notes })}>
-          <LuPencil
-            size={20}
-            className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
-            title="Edit"
-          />
-        </button>
-        <button onClick={() => onDelete(id)}>
-          <FaRegTrashAlt
-            size={20}
-            className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
-            title="Delete"
-          />
-        </button>
+
+          <button onClick={() => onDelete(id)}>
+            <FaRegTrashAlt
+              size={20}
+              className="text-deepOrange cursor-pointer hover:scale-105 transition-transform duration-200"
+              title="Delete"
+            />
+          </button>
+        </div>
       </div>
+
+      {showNotes && (
+        <div className="w-full pt-4">
+          <p className="font-semibold text-deepOrange">Notes</p>
+          <p className="whitespace-pre-line wrap-break-word">{notes}</p>
+        </div>
+      )}
     </div>
   );
 };
