08a2c574a3f71d66e6d341930462c9773a94012a
Shweta Tiwari <krishnast16@gmail.com>
2025-11-25 18:57:58 -0500

    Implement Top four saved and recently added jobs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import JobApplication from "../components/JobApplication";
-import SavedJob from "../components/SavedJob";
+import TopSavedJobs from "../components/TopSavedJobs";
 import Navbar from "../components/Navbar";
+import TopRecentJobs from "../components/TopRecentJobs";
 import MotivationBuddy from "../components/MotivationBuddy";
 
 const Home = () => {
@@ -10,47 +10,20 @@ const Home = () => {
 
       <main className="flex flex-row gap-8 items-start">
         {/* Jobs Dashboard */}
-        <div className="w-3/5 rounded-3xl bg-linear-to-r from-gray-50 to-white shadow-md p-8">
+
+        <div className="w-3/5 rounded-[25px] bg-[#FBF9F3] shadow-md p-8">
           {/* Recent Applications Section */}
           <div className="flex flex-col">
-            <h2 className="font-lora font-medium text-2xl mb-8">
-              Recently Applied
-            </h2>
-
-            <div className="w-full flex border-b-2 border-gray-300 pb-2 mb-4">
-              <p className="w-1/4 font-lora font-semibold text-lg">Company</p>
-              <p className="w-1/4 font-lora font-semibold text-lg">Role</p>
-              <p className="w-1/4 font-lora font-semibold text-lg">
-                Date Applied
-              </p>
-              <p className="w-1/4 font-lora font-semibold text-lg">Status</p>
-            </div>
-
-            <JobApplication />
-            <JobApplication />
-            <JobApplication />
-            <JobApplication />
+            <TopRecentJobs />
           </div>
-
           {/* Saved Jobs Section */}
           <div className="flex flex-col mt-8">
-            <h2 className="font-lora font-medium text-2xl mb-8">Saved Jobs</h2>
-
-            <div className="w-full flex border-b-2 border-gray-300 pb-2 mb-4">
-              <p className="w-1/3 font-lora font-semibold text-lg">Company</p>
-              <p className="w-1/3 font-lora font-semibold text-lg">Role</p>
-              <p className="w-1/3 font-lora font-semibold text-lg">Link</p>
-            </div>
-
-            <SavedJob />
-            <SavedJob />
-            <SavedJob />
-            <SavedJob />
+            <TopSavedJobs />
           </div>
         </div>
 
         {/* Motivation Buddy Component */}
-         <MotivationBuddy />
+        <MotivationBuddy />
       </main>
     </div>
   );
