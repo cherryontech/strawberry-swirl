88e5e4b5c0f23199c679597f722c5efbc62307c0
Shweta Tiwari <krishnast16@gmail.com>
2025-11-25 21:18:30 -0500

    Resolve Conflicts

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -18,7 +18,7 @@ const Resources = () => {
     <div className="bg-backgroundColor min-h-screen px-30 py-8">
       <Navbar />
 
-      <div className="flex flex-row items-center justify-center mt-5 ml-auto mr-auto gap-15">
+      <div className="flex flex-row items-center justify-center mt-5 ml-auto mr-auto gap-10">
         {Object.keys(jobLinksData).map((key) => (
           <FilterButton
             key={uuidv4()}
@@ -34,10 +34,10 @@ const Resources = () => {
       </div>
 
       <div className="flex flex-row h-screen mt-8 ml-auto mr-auto bg-[#E2CAB9] min-h-screen w-full rounded-[25px]">
-        <div className="flex flex-col items-center  justify-start mt-10 ml-auto mb-10 -mr-auto  w-[40%] bg-white rounded-[25px] overflow-y-auto scroll-smooth scrollbar-hide">
+        <div className="flex flex-col items-center  justify-start mt-10 ml-auto mb-10 -mr-auto  w-[45%] bg-white rounded-[25px] overflow-y-auto scroll-smooth scrollbar-hide">
           <JobBoards heading={selectedFilterButton} data={jobLinksData} />
         </div>
-        <div className="flex flex-col items-center justify-start mt-10 ml-auto mb-10 mr-auto w-[40%]  bg-white rounded-[25px] overflow-y-auto scroll-smooth scrollbar-hide">
+        <div className="flex flex-col items-center justify-start mt-10 ml-auto mb-10 mr-auto w-[45%]  bg-white rounded-[25px] overflow-y-auto scroll-smooth scrollbar-hide">
           <Article heading={selectedFilterButton} data={jobArticlesData} />
         </div>
       </div>
